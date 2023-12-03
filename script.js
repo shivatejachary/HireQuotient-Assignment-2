e90e2877ab7f5ad8e4b435c7d1f7e22155eb60cb
SIVA TEJACHARY JAYAVARAPU <98031864+shivatejachary@users.noreply.github.com>
2023-12-03 17:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,410 @@
+let mockData = [
+    {
+        "id": "1",
+        "name": "Aaron Miles",
+        "email": "aaron@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "2",
+        "name": "Aishwarya Naik",
+        "email": "aishwarya@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "3",
+        "name": "Arvind Kumar",
+        "email": "arvind@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "4",
+        "name": "Caterina Binotto",
+        "email": "caterina@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "5",
+        "name": "Chetan Kumar",
+        "email": "chetan@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "6",
+        "name": "Jim McClain",
+        "email": "jim@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "7",
+        "name": "Mahaveer Singh",
+        "email": "mahaveer@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "8",
+        "name": "Rahul Jain",
+        "email": "rahul@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "9",
+        "name": "Rizan Khan",
+        "email": "rizan@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "10",
+        "name": "Sarah Potter",
+        "email": "sarah@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "11",
+        "name": "Keshav Muddaiah",
+        "email": "keshav@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "12",
+        "name": "Nita Ramesh",
+        "email": "nita@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "13",
+        "name": "Julia Hunstman",
+        "email": "julia@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "14",
+        "name": "Juan Alonso",
+        "email": "juan@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "15",
+        "name": "Gabriel Montoya",
+        "email": "gabriel@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "16",
+        "name": "Beatrice Iglesias",
+        "email": "beatrice@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "17",
+        "name": "Sarah Symms",
+        "email": "sarah.s@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "18",
+        "name": "Patrick Pinheiro",
+        "email": "patrick@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "19",
+        "name": "Anand Patel",
+        "email": "anand@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "20",
+        "name": "Kishore Kalburgi",
+        "email": "kishore@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "21",
+        "name": "Rebecca Norris",
+        "email": "rebecca@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "22",
+        "name": "Özgür Başak",
+        "email": "ozgur@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "23",
+        "name": "Robin Andersen",
+        "email": "robin@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "24",
+        "name": "Nandini Kumar",
+        "email": "nandini@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "25",
+        "name": "Nikita Smith",
+        "email": "nikita@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "26",
+        "name": "Colton Doe",
+        "email": "colton@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "27",
+        "name": "Alain Senna",
+        "email": "alain@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "28",
+        "name": "Ashwin Jain",
+        "email": "ashwin@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "29",
+        "name": "Seema Bhatt",
+        "email": "seema@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "30",
+        "name": "Kayla Scarpinski",
+        "email": "kayla@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "31",
+        "name": "Ajay Ghosh",
+        "email": "ajay@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "32",
+        "name": "Chris Lindberg",
+        "email": "chris@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "33",
+        "name": "Christina Mourujärvi",
+        "email": "christina@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "34",
+        "name": "Mikhail Bill",
+        "email": "mikhail@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "35",
+        "name": "Eino Göregen",
+        "email": "eino@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "36",
+        "name": "Zachariah Johansson",
+        "email": "zacharaiah@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "37",
+        "name": "Aimaan Mohammed",
+        "email": "aimaan@mailinator.com",
+        "role": "admin"
+    },
+    {
+        "id": "38",
+        "name": "Aika Tsunoda",
+        "email": "aika@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "39",
+        "name": "Kimiko Minamoto",
+        "email": "kimiko@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "40",
+        "name": "Alyona Baginskaite",
+        "email": "alyona@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "41",
+        "name": "Anirudh Mukherjee",
+        "email": "anirudh@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "42",
+        "name": "Alyona Gov",
+        "email": "alyonagov@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "43",
+        "name": "Robin Singh",
+        "email": "robin@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "44",
+        "name": "Vijay Vasudevan",
+        "email": "vijayv@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "45",
+        "name": "Steve Smith",
+        "email": "steve@mailinator.com",
+        "role": "member"
+    },
+    {
+        "id": "46",
+        "name": "Anirudh Banerjee",
+        "email": "anirudhb@mailinator.com",
+        "role": "member"
+    }
+];
+
+let currentPage = 1;
+const itemsPerPage = 10;
+let selectedRows = [];
+
+function renderTable() {
+  const tableBody = document.getElementById("tableBody");
+  tableBody.innerHTML = "";
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedData = mockData.slice(startIndex, endIndex);
+
+  paginatedData.forEach(user => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td><input type="checkbox" onchange="selectRow('${user.id}', this)"></td>
+      <td>${user.id}</td>
+      <td>${user.name}</td>
+      <td>${user.email}</td>
+      <td>${user.role}</td>
+      <td>
+        <button class="edit" onclick="editRow('${user.id}')"><i class="far fa-edit"></i></button>
+        <button class="delete" onclick="deleteRow('${user.id}')"><i class="far fa-trash-alt"></i></button>
+      </td>
+    `;
+
+    if (selectedRows.includes(user.id)) {
+      row.classList.add("selected");
+      row.querySelector('input[type="checkbox"]').checked = true;
+    }
+
+    tableBody.appendChild(row);
+  });
+
+  const maxPage = Math.ceil(mockData.length / itemsPerPage);
+  document.getElementById("currentPage").innerText = `Page ${currentPage} of ${maxPage}`;
+  updateSelectedCount();
+}
+
+function selectRow(userId, checkbox) {
+  const isChecked = checkbox.checked;
+
+  if (isChecked) {
+    selectedRows.push(userId);
+  } else {
+    const index = selectedRows.indexOf(userId);
+    if (index > -1) {
+      selectedRows.splice(index, 1);
+    }
+  }
+
+  updateSelectedCount();
+}
+
+function updateSelectedCount() {
+  const selectedCount = document.querySelector('.selected-count');
+  selectedCount.innerText = `${selectedRows.length} row(s) selected`;
+
+  const selectedIDs = document.querySelector('.selected-ids');
+  selectedIDs.innerText = selectedRows.join(', ');
+}
+
+function search() {
+  const searchInput = document.getElementById("searchInput").value.toLowerCase();
+  mockData = mockData.filter(user =>
+    Object.values(user).some(value => value.toLowerCase().includes(searchInput))
+  );
+  currentPage = 1;
+  renderTable();
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    renderTable();
+  }
+}
+
+function nextPage() {
+  const maxPage = Math.ceil(mockData.length / itemsPerPage);
+  if (currentPage < maxPage) {
+    currentPage++;
+    renderTable();
+  }
+}
+
+function goToFirstPage() {
+  currentPage = 1;
+  renderTable();
+}
+
+function goToLastPage() {
+  const maxPage = Math.ceil(mockData.length / itemsPerPage);
+  currentPage = maxPage;
+  renderTable();
+}
+
+function editRow(userId) {
+  const userIndex = mockData.findIndex(user => user.id === userId);
+
+  const newName = prompt("Enter new name:");
+  const newEmail = prompt("Enter new email:");
+  const newRole = prompt("Enter new role:");
+
+  if (newName && newEmail && newRole) {
+    mockData[userIndex].name = newName;
+    mockData[userIndex].email = newEmail;
+    mockData[userIndex].role = newRole;
+
+    renderTable();
+    console.log(`Details for user with ID ${userId} have been edited`);
+  } else {
+    console.log("Edit operation canceled or invalid input");
+  }
+}
+
+function deleteRow(userId) {
+  const userIndex = mockData.findIndex(user => user.id === userId);
+
+  const confirmDelete = confirm(`Are you sure you want to delete user ${mockData[userIndex].name}?`);
+
+  if (confirmDelete) {
+    mockData.splice(userIndex, 1);
+    renderTable();
+    console.log(`User with ID ${userId} has been deleted`);
+  } else {
+    console.log("Delete operation canceled");
+  }
+}
+
+renderTable();
